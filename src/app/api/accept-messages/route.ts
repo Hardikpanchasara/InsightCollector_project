87fd0ca278b3589a8984104aee8901278b3a81ef
techerudite-hardik-panchasara <hardik.panchasara@techerudite.com>
2025-01-06 19:00:51 +0530

    .

--- src/app/api/accept-messages/route.ts
+++ src/app/api/accept-messages/route.ts
@@ -9,9 +9,9 @@ export async function POST(request: Request) {
     await dbConnect();
 
     const session = await getServerSession(authOptions);
-    const user: User = session?.user;
+    const user: User = session?.user as User
 
-    if(!session || !session.user) {
+    if (!session || !session.user) {
         return Response.json({
             success: false,
             message: "Not Authenticated"
@@ -19,4 +19,43 @@ export async function POST(request: Request) {
             status: 401
         })
     }
+
+    const userId = user._id;
+    const { acceptmessages } = await request.json()
+
+    try {
+        const updatedUser = await UserModel.findByIdAndUpdate(
+            userId,
+            { isAcceptingMessage: acceptmessages },
+            { new: true }
+        )
+        if (!updatedUser) {
+            return Response.json({
+                success: false,
+                message: "falied to update user status to accept messages"
+            }, {
+                status: 401
+            })
+        }
+
+        return Response.json({
+            success: true,
+            message: "Message acceptance status updated successfully",
+            updatedUser
+        }, {
+            status: 200
+        })
+    } catch (error) {
+        console.log("falied to update user status to accept messages")
+        return Response.json({
+            success: false,
+            message: "falied to update user status to accept messages"
+        }, {
+            status: 500
+        })
+    }
+}
+
+export async function GET(request: Request) {
+    
 }
