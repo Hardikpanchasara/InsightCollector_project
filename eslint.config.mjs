978a7162232c4b97757daf864fc5e3460beb77d6
techerudite-hardik-panchasara <hardik.panchasara@techerudite.com>
2025-01-21 12:29:23 +0530

    changes for build generation.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,19 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  // {
+  //   rules: {
+  //     '@typescript-eslint/no-unused-vars': [
+  //       'warn', // or 'off' to disable the rule completely
+  //       {
+  //         args: 'after-used', // Don't warn for unused function arguments
+  //         argsIgnorePattern: '^_', // Ignore variables starting with _
+  //         ignoreRestSiblings: true, // Ignore variables that are part of destructuring
+  //       },
+  //     ],
+  //     '@typescript-eslint/no-explicit-any': 'warn', // Change to 'warn' or 'off' for flexibility
+  //   },
+  // },
 ];
 
 export default eslintConfig;
